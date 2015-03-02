c9563e62bf9af9172b91f26d975a28e8fc02023c
Chris Marshall <chris@chrismar035.com>
2015-03-02 18:48:10 -0500

    SAVEPOINT

--- decoder.go
+++ decoder.go
@@ -1,15 +1,23 @@
 package drum
 
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+)
+
 // DecodeFile decodes the drum machine file found at the provided path
 // and returns a pointer to a parsed pattern which is the entry point to the
 // rest of the data.
 // TODO: implement
 func DecodeFile(path string) (*Pattern, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return &Pattern{}, err
+	}
+	reader := bufio.NewReader(file)
+
 	p := &Pattern{}
+	binary.Read(reader, binary.LittleEndian, &p)
 	return p, nil
 }
-
-// Pattern is the high level representation of the
-// drum pattern contained in a .splice file.
-// TODO: implement
-type Pattern struct{}
--- drum.go
+++ drum.go
@@ -1,3 +1,52 @@
 // Package drum is supposed to implement the decoding of .splice drum machine files.
 // See golang-challenge.com/go-challenge1/ for more information
 package drum
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// Pattern is the high level representation of the
+// drum pattern contained in a .splice file.
+// TODO: implement
+type Pattern struct {
+	version string
+	tempo   int
+	tracks  []track
+}
+
+type track struct {
+	id    int
+	name  string
+	steps []bool
+}
+
+type step struct {
+	active bool
+}
+
+// Default string representation of Pattern
+func (p *Pattern) String() string {
+	var buffer bytes.Buffer
+
+	var template = "Saved with HW Version: %s\nTempo: %d"
+	buffer.WriteString(fmt.Sprintf(template, p.version, p.tempo))
+	for _, track := range p.tracks {
+		buffer.WriteString(fmt.Sprintf("\n%v", track))
+	}
+	return buffer.String()
+}
+
+func (t *track) String() string {
+	var template = "(%d) %s\t|%s%s%s%s|%s%s%s%s|%s%s%s%s|%s%s%s%s|"
+	return fmt.Sprintf(template, t.id, t.name, t.steps)
+}
+
+func (s *step) String() string {
+	if s.active {
+		return "x"
+	} else {
+		return "-"
+	}
+}
